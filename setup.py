7df7791bfd9552bd4cae337197ec11754dc7454d
Kyle Montemayor <kmonte@umd.edu>
2021-02-24 10:14:00 -0500

    remove other platforms so poetry compatibility

--- setup.py
+++ setup.py
@@ -29,71 +29,13 @@ eager_resources = list()
 zip_safe = True
 
 
-if sys.platform == 'win32':
-    ext_modules.append(Extension('bluetooth._msbt',
-                       libraries=["WS2_32", "Bthprops"],
-                       sources=['msbt\\_msbt.c']))
-
-    # widcomm
-    WC_BASE = os.path.join(os.getenv('ProgramFiles'), r"Widcomm\BTW DK\SDK")
-    if os.path.exists(WC_BASE):
-        ext_modules.append(Extension('bluetooth._widcomm',
-                include_dirs=["%s\\Inc" % WC_BASE],
-                define_macros=[('_BTWLIB', None)],
-                library_dirs=["%s\\Release" % WC_BASE],
-                libraries=["WidcommSdklib", "ws2_32", "version", "user32",
-                           "Advapi32", "Winspool", "ole32", "oleaut32"],
-                sources=["widcomm\\_widcomm.cpp",
-                         "widcomm\\inquirer.cpp",
-                         "widcomm\\rfcommport.cpp",
-                         "widcomm\\rfcommif.cpp",
-                         "widcomm\\l2capconn.cpp",
-                         "widcomm\\l2capif.cpp",
-                         "widcomm\\sdpservice.cpp",
-                         "widcomm\\util.cpp"]))
-
-elif sys.platform.startswith('linux'):
+if sys.platform.startswith('linux'):
     mod1 = Extension('bluetooth._bluetooth',
                      libraries = ['bluetooth'],
                      #extra_compile_args=['-O0'],
                      sources = ['bluez/btmodule.c', 'bluez/btsdp.c'])
     ext_modules.append(mod1)
 
-elif sys.platform.startswith("darwin"):
-    packages.append('lightblue')
-    package_dir['lightblue'] = 'macos'
-    zip_safe = False
-
-    if sys.version_info >= (3,6):
-        install_requires += ['pyobjc-core>=6', 'pyobjc-framework-Cocoa>=6']
-    else:
-        install_requires += ['pyobjc-core>=3.1,<6', 'pyobjc-framework-Cocoa>=3.1,<6']
-
-    # FIXME: This is inelegant, how can we cover the cases?
-    build_cmds = {'bdist', 'bdist_egg', 'bdist_wheel'}
-    if build_cmds & set(sys.argv):
-        # Build the framework into macos/
-        import subprocess
-        subprocess.check_call([
-            'xcodebuild', 'install',
-            '-project', 'macos/LightAquaBlue/LightAquaBlue.xcodeproj',
-            '-scheme', 'LightAquaBlue',
-            'DSTROOT=' + os.path.join(os.getcwd(), 'macos'),
-            'INSTALL_PATH=/',
-            'DEPLOYMENT_LOCATION=YES',
-        ])
-        
-        # We can't seem to list a directory as package_data, so we will
-        # recursively add all all files we find
-        package_data['lightblue'] = []
-        for path, _, files in os.walk('macos/LightAquaBlue.framework'):
-            for f in files:
-                include = os.path.join(path, f)[6:]  # trim off macos/
-                package_data['lightblue'].append(include)
-    
-        # This should allow us to use the framework from an egg [untested]
-        eager_resources.append('macos/LightAquaBlue.framework')
-        
 else:
     raise Exception("This platform (%s) is currently not supported by pybluez."
                     % sys.platform)
